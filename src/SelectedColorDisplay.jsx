ad931cf8ce108050a2dd8dcc3e6b7db164ed030d
wangbertha <wangbertha@gmail.com>
2024-09-24 13:28:28 -0500

    Add default message for no selected color

--- src/SelectedColorDisplay.jsx
+++ src/SelectedColorDisplay.jsx
@@ -1,6 +1,12 @@
 import './SelectedColorDisplay.css'
 
 const SelectedColorDisplay = ({ selectedColor }) => {
+  if (!selectedColor) {
+    return (
+      <p className='selected-color-display'>Currently selected: <span className='initial-message'>Please select a color</span></p>
+    )
+  }
+
   return (
     <p className='selected-color-display'>Currently selected: {selectedColor}</p>
   )
